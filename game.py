99fed85e731ce2e5f222fd41361b2ba336f25029
Resham Singh <2007resham@gmail.com>
2025-12-27 22:45:55 +0530

    Check winner function

---
+++ game.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def check_winner():
+    for combos in [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]:
+        if buttons[combos[0]]["Text"] == buttons[comobs[1]]["Text"] == buttons[combos[2]]["Text"] != "":
+            buttons[combos[0]].config(bg="green")
+            buttons[combos[1]].config(bg="green")
+            buttons[combos[2]].config(bg="green")
+            messagebox.showinfo("TIC-TAC-Toe",f"Player {buttons[combos[0]]["Text"]} wins!")
